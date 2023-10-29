06880dcc539d1899d8d1b3a873e23fa21515f204
iperez13 <iperez13@ucol.mx>
2023-10-29 13:19:02 -0600

    second commit

--- app.js
+++ app.js
@@ -26,30 +26,111 @@
 // }
 
 
-let dia = prompt("dame el dia de la semana").toLowerCase();
-switch (dia) {
-    case 'lunes':
-        console.log("el dia que seleccionaste fue el dia lunes");  
-        break;
-    case 'martes':
-        console.log("el dia que seleccionaste fue el dia martes");  
-        break;
-    case 'miercoles':
-            console.log("el dia que seleccionaste fue el dia miercoles");  
-        break;
-    case 'jueves':
-        console.log("el dia que seleccionaste fue el dia jueves");  
-        break;
-    case 'viernes':
-        console.log("el dia que seleccionaste fue el dia viernes");  
-        break;
-    case 'sabado':
-        console.log("el dia que seleccionaste fue el dia sabado");  
-        break;
-    case 'domingo':
-        console.log("el dia que seleccionaste fue el dia domingo");  
-        break;
+// let dia = prompt("dame el dia de la semana").toLowerCase();
+// switch (dia) {
+//     case '1':
+//         console.log("el dia que seleccionaste fue el dia lunes");  
+//         break;
+//     case 'martes':
+//         console.log("el dia que seleccionaste fue el dia martes");  
+//         break;
+//     case 'miercoles':
+//             console.log("el dia que seleccionaste fue el dia miercoles");  
+//         break;
+//     case 'jueves':
+//         console.log("el dia que seleccionaste fue el dia jueves");  
+//         break;
+//     case 'viernes':
+//         console.log("el dia que seleccionaste fue el dia viernes");  
+//         break;
+//     case 'sabado':
+//         console.log("el dia que seleccionaste fue el dia sabado");  
+//         break;
+//     case 'domingo':
+//         console.log("el dia que seleccionaste fue el dia domingo");  
+//         break;
 
-    default: console.log("dia de la semana no valido");
+//     default: console.log("dia de la semana no valido");
+//         break;
+// }
+
+// OPERADOR TERNARIO
+
+// let numero1 = 15;
+// let numero2 = 20;
+//                     // condicion ? true : false;
+// let valor = (numero1 > numero2) ? "el numero1 es mayor" : "el numero2 es mayor";
+
+// console.log(valor);
+
+
+// Ejercicio 1
+// let numero1 = prompt("escribe un numero");
+// let numero2 = prompt("escribe otro numero");
+
+// if (numero1 === numero2) {
+//     console.log("Los numeros son iguales, ");
+//     document.write("Los numeros son iguales, ");
+// } else {
+//     console.log("los numeros son diferentes, ");
+//     document.write("Los numeros son diferentes, ");
+// }
+// if (numero1 > numero2) {
+//     console.log(numero1 + " es mayor " );
+//     document.write(numero1 + " es mayor " );
+// } else {
+//     console.log(numero2 + " es mayor " );
+//     document.write(numero2 + " es mayor " );
+// } 
+
+
+// Ejercicio 2
+// let numero = prompt("dame un numero");
+// if (numero % 2 === 0) {
+//     console.log("el numero es par");
+//     document.write("el numero es par");
+// } else {
+//     console.log("el numero es impar");
+//     document.write("el numero es impar");
+// }
+
+// Ejercicio 3
+// let numero1 = prompt("dame un numero entero")
+// let numero2 =  prompt("dame otro numero entero")
+// if (numero1 == 0 && numero2 == 0) {
+//         document.write("error")
+//         console.log("error")
+// }else if (numero1 > 0 && numero2 > 0) {
+//         document.write("El cociente es " + numero1/numero2);
+//         console.log("El cociente es " + numero1/numero2);
+// }
+
+// Ejercicio 4
+let numero1 = prompt("dame un numero");
+let numero2 = prompt("dame otro numero");
+
+numero1 = parseFloat(numero1);
+numero2 = parseFloat(numero2);
+
+
+let operaciones = prompt("¿Qué operación quieres hacer?", "suma, resta, multiplicacion, division");
+switch (operaciones) {
+    case 'suma':
+        console.log(numero1 + numero2);
+        break;
+    case 'resta':
+        console.log(numero1 - numero2);
+        break;
+    case 'multiplicacion':
+        console.log(numero1 * numero2);
+        break;
+    case 'division':
+        if (numero1 == 0 && numero2 == 0) {
+            console.log("no se puede hacer la división");
+        }else{
+            console.log(numero1 / numero2);
+        }
+        break;
+    default:
         break;
 }
